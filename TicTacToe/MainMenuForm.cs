4933aeb22c545b598f082f5fe606d0c4d677bded
fisaxili <anfisavoro@gmail.com>
2026-04-17 01:36:22 +0300

    В форме главного меню добавлен обработчик кнопок для выбора режима игры

--- TicTacToe/MainMenuForm.cs
+++ TicTacToe/MainMenuForm.cs
@@ -1,20 +1,51 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows.Forms;
+﻿using TicTacToe;
+using TicTacToe.Logic;
 
-namespace TicTacToe
+namespace TicTacToe;
+
+/// <summary>
+/// Главная форма — начальный экран приложения.
+/// Позволяет выбрать режим игры: один игрок против компьютера или два игрока.
+/// </summary>
+public partial class MainMenuForm : Form
 {
-    public partial class MainMenuForm : Form
+    /// <summary>
+    /// Инициализирует компоненты главного меню.
+    /// </summary>
+    public MainMenuForm()
     {
-        public MainMenuForm()
+        InitializeComponent();
+    }
+
+    /// <summary>
+    /// Обработчик кнопки "Один игрок".
+    /// Открывает форму настройки для режима игрок против компьютера.
+    /// </summary>
+    private void btnSinglePlayer_Click(object sender, EventArgs e)
+    {
+        using var setup = new PlayerSetupForm(isTwoPlayers: false);
+        if (setup.ShowDialog() == DialogResult.OK && setup.GameEngine != null)
         {
-            InitializeComponent();
+            var gameForm = new GameForm(setup.GameEngine);
+            gameForm.FormClosed += (_, _) => Show();
+            Hide();
+            gameForm.Show();
+        }
+    }
+
+    /// <summary>
+    /// Обработчик кнопки "Два игрока".
+    /// Открывает форму настройки для режима игрок против игрока.
+    /// </summary>
+    private void btnTwoPlayers_Click(object sender, EventArgs e)
+    {
+        using var setup = new PlayerSetupForm(isTwoPlayers: true);
+        if (setup.ShowDialog() == DialogResult.OK && setup.GameEngine != null)
+        {
+            var gameForm = new GameForm(setup.GameEngine);
+            gameForm.FormClosed += (_, _) => Show();
+            Hide();
+            gameForm.Show();
         }
     }
 }
